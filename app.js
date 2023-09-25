41468b3923763803e25769eb220e2dce03d293e7
jorge.flores <jorgefr_1693@hotmail.com>
2023-09-25 11:47:04 -0600

    Trabajando con RestApi

--- app.js
+++ app.js
@@ -1,22 +1,79 @@
+const path = require('path');
+
+const cors = require('cors');
+
 const express = require('express');
+const mongoose = require('mongoose');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function(req, file, cb) {
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 // Para quitar el error del navegador de favicon
 app.get('/favicon.ico', (req, res) => res.status(204));
 
 // app.use(express.urlencoded({ extended: true }));  // x-www-form-urlencoded <form>
 app.use(express.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+
+// Probando Si es problema de CORS
+// app.options('*', cors());
+// app.use(cors());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', '*');
   next();
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(
+    'mongodb+srv://jorgeNode:prueba123456@cluster0.gmxutxg.mongodb.net/messages?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
