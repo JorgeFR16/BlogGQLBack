9f21b8f4899dcd665472d761454854140585e62f
jorge.flores <jorgefr_1693@hotmail.com>
2023-09-26 13:56:12 -0600

    Implementando Socket.io

--- app.js
+++ app.js
@@ -76,6 +76,10 @@ mongoose
     process.env.SECRET_1
   )
   .then(result => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require('./socket').init(server);
+    io.on('connection', socket => {
+      console.log('Client connected');
+    });
   })
   .catch(err => console.log(err));
