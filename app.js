59a8274fb92b7de2616e3416eb096ba00a00bcb1
jorge.flores <jorgefr_1693@hotmail.com>
2023-11-09 17:07:21 -0600

    Se termina la aplicacion utilizando GraphQL

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-const path = require('path');
+const path = require("path");
 
-const cors = require('cors');
+const cors = require("cors");
 
-const express = require('express');
-const mongoose = require('mongoose');
-const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
-const { graphqlHTTP } = require('express-graphql');
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const { v4: uuidv4 } = require("uuid");
+const { graphqlHTTP } = require("express-graphql");
 
-const graphqlSchema = require('./graphql/schema');
-const graphqlResolver = require('./graphql/resolvers');
-const auth = require('./middleware/auth');
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolver = require("./graphql/resolvers");
+const auth = require("./middleware/auth");
+const { clearImage } = require('./util/file');
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 const fileStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'images');
+    cb(null, "images");
   },
   filename: function (req, file, cb) {
     cb(null, uuidv4());
@@ -27,9 +28,9 @@ const fileStorage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg' ||
-    file.mimetype === 'image/jpeg'
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
   ) {
     cb(null, true);
   } else {
@@ -38,27 +39,27 @@ const fileFilter = (req, file, cb) => {
 };
 
 // Para quitar el error del navegador de favicon
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.get("/favicon.ico", (req, res) => res.status(204));
 
 // app.use(express.urlencoded({ extended: true }));  // x-www-form-urlencoded <form>
 app.use(express.json()); // application/json
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // Probando Si es problema de CORS
 // app.options('*', cors());
 // app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  if (req.method === 'OPTIONS') {
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
   next();
@@ -66,8 +67,23 @@ app.use((req, res, next) => {
 
 app.use(auth);
 
+app.put("/post-image", (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: "No file provided!" });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: "File stored.", filePath: req.file.path.replace(/\\/g, "/") });
+});
+
 app.use(
-  '/graphql',
+  "/graphql",
   graphqlHTTP({
     schema: graphqlSchema,
     rootValue: graphqlResolver,
@@ -77,10 +93,10 @@ app.use(
         return err;
       }
       const data = err.originalError.data;
-      const message = err.message || 'An error ocurred.';
+      const message = err.message || "An error ocurred.";
       const code = err.originalError.code || 500;
       return { message: message, status: code, data: data };
-    }
+    },
   })
 );
 
@@ -94,7 +110,8 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(process.env.SECRET_1)
-  .then(result => {
+  .then((result) => {
     app.listen(8080);
   })
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
+
